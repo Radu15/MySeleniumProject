0509f1d31cb1619f9bd7e95b86b93b03c327999b
RADU <radu_chis59@yahoo.com>
2024-01-23 23:36:25 +0200

    ok

---
+++ src/test/java/com/demoqa/RadioTestButon.java
@@ -0,0 +1,52 @@
+package com.demoqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RadioTestButon {
+        WebDriver driver;
+        String url = "https://demoqa.com/radio-button";
+
+        @BeforeTest
+        public void setup() {
+            System.setProperty("webdriver.chrome.driver",
+                    "src/main/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.get(url);
+            driver.manage().window().maximize();
+        }
+
+        @Test
+        public void testRadiobuttonFunctionality() {
+            // Deschide pagina web
+            driver.get(url);
+
+            // Identifică elementele radio button
+            WebElement yesRadio = driver.findElement(By.xpath("//label[@for='yesRadio']"));
+            WebElement impressiveRadio = driver.findElement(By.xpath("//label[@for='impressiveRadio']"));
+
+            // Verifică dacă opțiunea "Yes" este bifată și dezactivează-o
+            Assert.assertFalse(yesRadio.isSelected());
+            yesRadio.click();
+            Assert.assertFalse(yesRadio.isSelected());
+
+            // Verifică dacă opțiunea "Impressive" nu este bifată și activează-o
+            Assert.assertFalse(impressiveRadio.isSelected());
+            impressiveRadio.click();
+            Assert.assertFalse(impressiveRadio.isSelected());
+        }
+
+    @AfterTest
+    public void tearDown() {
+        // Închide browser-ul la sfârșitul testului
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
